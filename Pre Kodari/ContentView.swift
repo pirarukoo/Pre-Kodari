7f8c1623d03f5a0acda8ccb8ba256a19e6746074
pirarukoo <168701666+pirarukoo@users.noreply.github.com>
2024-05-16 14:28:05 +0900

    Update ContentView.swift

--- Pre Kodari/ContentView.swift
+++ Pre Kodari/ContentView.swift
@@ -15,6 +15,7 @@ struct ContentView: View {
                 .foregroundStyle(.tint)
             Text("Hello, world!")
             Text("first text")
+            Text("github edit")
         }
         .padding()
     }
