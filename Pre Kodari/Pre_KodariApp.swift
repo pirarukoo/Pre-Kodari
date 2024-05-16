a5ece3f6208784aa5a13d260f4ea5844cbaaa1e8
pirarukoo <sanim24@pos.idserve.net>
2024-05-16 14:19:34 +0900

    Initial Commit

---
+++ Pre Kodari/Pre_KodariApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pre_KodariApp.swift
+//  Pre Kodari
+//
+//  Created by sanghyuk on 5/16/24.
+//
+
+import SwiftUI
+
+@main
+struct Pre_KodariApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
